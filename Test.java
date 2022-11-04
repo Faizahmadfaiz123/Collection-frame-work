b31e9b67842c5a3c438caa2cb3917c656a66a5ed
Faizahmadfaiz123 <117396443+Faizahmadfaiz123@users.noreply.github.com>
2022-11-04 13:45:48 -0700

    linkledist
    linklist is added

--- Test.java
+++ Test.java
@@ -1,27 +1,60 @@
 package pack;
 
-import java.awt.List;
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Test {
 
 	public static void main(String[] args) {
 		
-		ArrayList arr = new ArrayList();
-		arr.add(new stu(10, "alikhan"));
-		arr.add(new emp(1, "ahmad"));
-		arr.add(new stu(11, "jan"));
+		LinkedList list = new LinkedList();
+		list.add("Faiz");
+		list.add("Ali");
+		list.add(5000);
+		list.add(6000);
 		
-//		System.out.println(arr);
+		System.out.println(list);
+		
+		list.addFirst("khan");
+		System.out.println(list);
 		
 		
-//		System.out.println(arr.get(0));
-//		System.out.println(arr.get(1));
-//		System.out.println(arr.get(2));
+		list.addLast("walid");
+		System.out.println(list);
 		
-		emp e1 = (emp)arr.get(0);
-		System.out.println(e1.eId + " " + e1.eName);
-		emp o02 = (emp)arr.get(1);
 		
+		list.add(3, 100000);
+		System.out.println(list);
+		
+		list.add(1, 100000);
+		System.out.println(list);
+		
+		list.remove();
+		System.out.println(list);
+		
+		list.remove(3);
+		System.out.println(list);
+		
+		list.remove(0);
+		System.out.println(list);
+		
+		list.removeFirst();
+		System.out.println(list);
+		
+		
+		list.removeLast();
+		System.out.println(list);
+		
+		
+		
+		if(list.isEmpty()){
+		System.out.println("LinkedList is imPmty");
+		}else{
+			System.out.println("LinkedList is valid data...  ");
+		}
+		
+		
+		list.clear();
+		System.out.println(list);
 	}
+
 }
