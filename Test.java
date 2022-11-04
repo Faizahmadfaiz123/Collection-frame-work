d7a1d3d77fb010ca932c377a434ed0f86b419824
Faizahmadfaiz123 <117396443+Faizahmadfaiz123@users.noreply.github.com>
2022-11-04 13:49:57 -0700

    collection 01
    collection 01 is added

--- Test.java
+++ Test.java
@@ -1,73 +1,27 @@
 package pack;
 
-import java.util.Iterator;
-import java.util.Vector;
+import java.awt.List;
+import java.util.ArrayList;
 
 public class Test {
 
 	public static void main(String[] args) {
 		
-//		vector class has 4 constructer.
-//		default constructer.
-		Vector list = new Vector();
-		list.add("faiz");
-		list.add(50000);
-		list.add("Ahmad");
-		list.add(50000);
+		ArrayList arr = new ArrayList();
+		arr.add(new stu(10, "alikhan"));
+		arr.add(new emp(1, "ahmad"));
+		arr.add(new stu(11, "jan"));
 		
-		System.out.println(list);
-		
-//		for each loop.
-		for(Object obj: list){
-			Object o = (Object)obj;
-			System.out.println(o);
-		}
-		
-
-//		Iterator curser.
-//		user defined capacity constructer.
-		
-		Vector list01 = new Vector(5);
-		list01.add("faiz");
-		list01.add(50000);
-		list01.add("Ahmad");
-		list01.add(50000);
-		list01.add("AhmadKhan");
-		list01.add(50658);
-		list01.add("AhmadJan");
-		list01.add(5590);
+//		System.out.println(arr);
 		
 		
-		Iterator data = list01.iterator();
-		while(data.hasNext()){
-			Object obj = (Object)data.next();
-			System.out.println(obj);
-		}
-		System.out.println(list01.capacity());
+//		System.out.println(arr.get(0));
+//		System.out.println(arr.get(1));
+//		System.out.println(arr.get(2));
 		
-		
-		
-//		3 constrcter in the vectoer classes.
-		
-		System.out.println("/////////////////////////////////////");
-		
-		Vector list02 = new Vector(5, 8);
-		list02.add("faiz");
-		list02.add(50000);
-		list02.add("Ahmad");
-		list02.add(50000);
-		list02.add("AhmadKhan");
-		list02.add(50658);
-		list02.add("AhmadJan");
-		list02.add(5590);
-	
-		
-		Iterator data01 = list02.iterator();
-		while(data01.hasNext()){
-			Object ob = (Object)data01.next();
-			System.out.println(ob);
-		}
-		System.out.println(list02.capacity());
+		emp e1 = (emp)arr.get(0);
+		System.out.println(e1.eId + " " + e1.eName);
+		emp o02 = (emp)arr.get(1);
 		
 	}
 }
